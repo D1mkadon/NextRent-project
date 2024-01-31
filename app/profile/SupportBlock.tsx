2d5cac700e7cb96e903e4b8773044a4b70880bea
D1mkadon <dima0938157970@gmail.com>
2024-01-31 19:24:20 +0100

    adapt profile

--- app/profile/SupportBlock.tsx
+++ app/profile/SupportBlock.tsx
@@ -2,9 +2,9 @@ import { supportData } from "./supportLinksData";
 
 const SupportBlock = () => {
   return (
-    <div className="col-start-1 col-end-3 flex justify-between w-full items-center">
+    <div className="col-start-1 col-end-3 flex justify-between items-center md:flex-row flex-col w-full">
       {supportData?.map((e, index) => (
-        <div key={index} className="flex flex-col gap-3">
+        <div key={index} className="flex flex-col gap-3 w-full items-center">
           {e.Icon}
           <h4 className="capitalize font-bold">{e.title}</h4>
           <p>{e.description}</p>
